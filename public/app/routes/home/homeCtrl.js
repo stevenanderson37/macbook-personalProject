baca07977cd9353e36fcbbd6602ba39658d04d3a
Steven Anderson <stevenanderson@Stevens-MacBook-Pro.local>
2016-11-09 21:08:44 -0700

    used Cascading SVG Fill to fix the hover for the Design film button

--- public/app/routes/home/homeCtrl.js
+++ public/app/routes/home/homeCtrl.js
@@ -1,5 +1,10 @@
 angular.module("app")
 .controller('homeCtrl', function($scope, $stateParams, mainService) {
   $scope.stateName = 'Home';
-  
+
+  $(".full-state-navbar").stick_in_parent({
+    "parent": "body",
+    "bottoming": false
+  });
+
 })
