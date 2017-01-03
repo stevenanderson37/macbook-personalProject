ba6d6b96c59cb7be982adbf6502506ae1ee0176e
Steven Anderson <happygoatdesigns@gmail.com>
2017-01-02 23:15:04 -0700

    more sections of the wireless state completed, and improved styling and layout for bag

---
+++ public/app/routes/wireless/wirelessDirectives/wirelessAirplayDir.js
@@ -0,0 +1,10 @@
+angular.module("app")
+.directive('wirelessAirplayDir', function() {
+
+  return {
+    restrict: 'AE',
+    templateUrl: './app/routes/wireless/wirelessDirectives/wirelessAirplayTmpl.html'
+  }
+
+
+});
---
+++ public/app/routes/wireless/wirelessDirectives/wirelessBluetoothDir.js
@@ -0,0 +1,10 @@
+angular.module("app")
+.directive('wirelessBluetoothDir', function() {
+
+  return {
+    restrict: 'AE',
+    templateUrl: './app/routes/wireless/wirelessDirectives/wirelessBluetoothTmpl.html'
+  }
+
+
+});
