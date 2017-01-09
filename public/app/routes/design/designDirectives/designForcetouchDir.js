8de6e28fc42a04eb116aebb4a5e51eee29b52afa
Steven Anderson <happygoatdesigns@gmail.com>
2017-01-09 15:06:35 -0700

    styling done on design state up through section Forcetouch

---
+++ public/app/routes/design/designDirectives/designForcetouchDir.js
@@ -0,0 +1,10 @@
+angular.module("app")
+.directive('designForcetouchDir', function() {
+
+  return {
+    restrict: 'AE',
+    templateUrl: './app/routes/design/designDirectives/designForcetouchTmpl.html'
+  }
+
+
+});
